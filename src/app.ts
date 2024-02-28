a6e2537a28a414e818cbccf43d27628c21c0c605
Bruno Gianini <blgianini@gmail.com>
2024-02-28 02:00:24 -0300

    Cors allow PATCH

--- src/app.ts
+++ src/app.ts
@@ -24,7 +24,7 @@ AppDataSource.initialize().then(() => {
 
     app.use((req, res, next) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
         res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         next();
     });
